867e6a608d3f962a9e7d7b6652d15184b205440c
Bob <kerim-serdar@hotmail.com>
2021-06-05 15:01:47 +0300

    connection changed

--- lib/connect.dart
+++ lib/connect.dart
@@ -3,10 +3,10 @@ import 'package:mysql1/mysql1.dart';
 class Mysql {
   Future<MySqlConnection> getConnection() async {
     var settings = new ConnectionSettings(
-        host: '192.168.0.24',
+        host: '',
         port: 3306,
         user: 'root',
-        password: '2014101kerim',
+        password: '',
         db: 'data');
     return await MySqlConnection.connect(settings);
   }
